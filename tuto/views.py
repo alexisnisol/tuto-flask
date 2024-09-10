bc0f9d17cf7473aad3361e816e9ecc9dc0fb175a
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-09-10 15:27:00 +0200

    Ajout de la template livres et de sa route associée

--- tuto/views.py
+++ tuto/views.py
@@ -1,9 +1,22 @@
 from .app import app
 from flask import render_template
+from .models import get_books
+
 
 @app.route("/")
 def home():
+    return "<h1>Hello World!</h1>"
+
+@app.route("/names")
+def show_names():
     return render_template(
         "home.html",
         title="Hello World!",
         names=["Pierre", "Paul", "Corinne"])
+
+@app.route("/livres")
+def show_livres():
+    return render_template(
+        "livres.html",
+        title="Livres d'Amazon !",
+        books=get_books())
