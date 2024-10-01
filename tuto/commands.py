6f293e0d30859ef4a5f82dc43568573ea6cf5118
alexisnisol <alexis.nisol@etu.univ-orleans.fr>
2024-10-01 14:54:28 +0200

    Ajout de la commande syncdb

--- tuto/commands.py
+++ tuto/commands.py
@@ -37,3 +37,8 @@ def loaddb(filename):
             author_id= a.id)
         db.session.add(o)
     db.session.commit()
+
+@app.cli.command()
+def syncdb():
+    '''Creates all missing tables.'''
+    db.create_all()
